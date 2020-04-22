2c8147a3e429abd78a3b52e58c8374f29f9d8973
Andrzej Olszak <andrzej.olszak@gmail.com>
2020-04-22 23:27:18 +0200

    Changed analysis description text

--- integration/events.go
+++ integration/events.go
@@ -176,7 +176,7 @@ func (s SlackHandler) handleMoveCommand(gameID string, moveCommand *MoveCommand,
 	pgnAttachment := slack.Attachment{
 		Title:     "Analysis",
 		TitleLink: s.Hostname + "/analyze?game_id=" + gm.ID,
-		Text:      gm.Export(),
+		Text:      "(select Run Report -> Basic)",
 	}
 
 	if outcome := gm.Outcome(); outcome != chess.NoOutcome {
